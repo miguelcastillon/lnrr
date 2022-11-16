53876e72116353834d2240290f4fa9f2c183a81c
miguelcastillon <mcastillonsanchez@gmail.com>
2022-11-16 11:53:23 +0100

    Changed class types

--- include/lnrr_se3/scan_to_model.h
+++ include/lnrr_se3/scan_to_model.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <lnrr/ceres/cost_U_scan_to_model.h>
-#include <lnrr/utils/conversions.h>
-#include <lnrr/utils/types.h>
-#include <lnrr/utils/utils.h>
+#include <lnrr_se3/ceres/cost_W.h>
+#include <lnrr_se3/utils/conversions.h>
+#include <lnrr_se3/utils/operations.h>
+#include <lnrr_se3/utils/types.h>
 
 #include <fgt.hpp>
 
@@ -21,22 +21,11 @@ private:
     MatrixX3 fixed_;
     MatrixX3 moving_;
     MatrixX3 moving_transformed_;
-    SparseMatrix YD_;
+
+    Probabilities P_;
 
     Matrix G_;
-    SparseMatrix F_;
-    SparseMatrix FT_;
-    Matrix FG_;
-    SparseMatrix H_;
-
-    MatrixX3 A_;
-    Matrix B_;
-    Matrix GB_;
-    MatrixX3 C_;
-    SparseMatrix D_;
-
-    MatrixX3 U_; // rotation
-    MatrixX3 V_; // translation
+    MatrixX6 W_;
 
     MatrixX3 RT_; // tranpose of the rotation matrices
 
@@ -45,7 +34,7 @@ private:
 
     double beta_;
     double lambda_;
-    Vector line_sizes_;
+    VectorInt line_sizes_;
     int number_lines_;
     size_t max_iterations_;
     double outliers_;
@@ -55,13 +44,12 @@ private:
 
     double defaultSigma2();
     void computeSigma2();
-    void computeU();
-    void computeOptimalRotationCeres(const Matrix& S, const Matrix& T);
+    void computeW();
 
 public:
     ScanToModel(const MatrixX3& fixed, const MatrixX3& moving,
                 const double& beta, const double& lambda,
-                const Vector& line_sizes)
+                const VectorInt& line_sizes)
         : fixed_(fixed),
           moving_(moving),
           beta_(beta),
@@ -80,20 +68,20 @@ public:
 
     ~ScanToModel() {}
 
-    Probabilities P_;
-
     void initialize();
     void computeP();
     void computeP_FGT();
     void computeOne();
-    Matrix getTransformedMoving();
+    MatrixX3 getTransformedMoving() {
+        return computeTransformedMoving(moving_, G_, W_, line_sizes_);
+    }
     Result run();
 
     // Set functions
 
     void setFixed(const MatrixX3& fixed) { fixed_ = fixed; }
     void setMoving(const MatrixX3& moving) { moving_ = moving; }
-    void setLineSizes(const Vector& line_sizes) {
+    void setLineSizes(const VectorInt& line_sizes) {
         line_sizes_ = line_sizes;
         number_lines_ = line_sizes.rows();
     }
@@ -111,10 +99,10 @@ public:
     void setTolerance(const double& tolerance) { tolerance_ = tolerance; }
 
     // Get functions
-
+    Probabilities getP() { return P_; }
     MatrixX3 getFixed() { return fixed_; }
     MatrixX3 getMoving() { return moving_; }
-    Vector getLineSizes() { return line_sizes_; }
+    VectorInt getLineSizes() { return line_sizes_; }
     int getNumberLines() { return number_lines_; }
     double getBeta() { return beta_; }
     double getLambda() { return lambda_; }
---
+++ include/lnrr_se3/utils/types.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+#include <chrono>
+
+namespace lnrr {
+
+class lnrr_error : public std::runtime_error {
+public:
+    explicit lnrr_error(const std::string what_arg)
+        : std::runtime_error(what_arg) {}
+};
+
+typedef Eigen::MatrixXd Matrix;
+typedef Eigen::SparseMatrix<double> SparseMatrix;
+typedef Eigen::Matrix3d Matrix3;
+typedef Eigen::MatrixX3d MatrixX3;
+typedef Eigen::Matrix<double, Eigen::Dynamic, 6> MatrixX6;
+typedef Eigen::VectorXd Vector;
+typedef Eigen::Matrix<int, Eigen::Dynamic, 1> VectorInt;
+typedef Eigen::Vector3d Vector3;
+
+template <typename T>
+using MatrixT = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+template <typename T>
+using Matrix3T = Eigen::Matrix<T, 3, 3>;
+template <typename T>
+using MatrixX3T = Eigen::Matrix<T, Eigen::Dynamic, 3>;
+template <typename T>
+using MatrixX6T = Eigen::Matrix<T, Eigen::Dynamic, 6>;
+template <typename T>
+using VectorT = Eigen::Matrix<T, Eigen::Dynamic, 1>;
+template <typename T>
+using Vector3T = Eigen::Matrix<T, 3, 1>;
+
+template <typename T>
+struct RigidTransform {
+    Vector3T<T> xyz;
+    Matrix3T<T> rot_mat;
+};
+
+struct Result {
+    MatrixX3 points;
+    std::vector<RigidTransform<double>> line_transforms;
+    double sigma2;
+    std::chrono::microseconds runtime;
+    size_t iterations;
+};
+
+struct Probabilities {
+    Vector p1;
+    Vector pt1;
+    Matrix px;
+    double l;
+};
+} // namespace lnrr
